9ee382ea895bb62691c7a96f95bdbd68634eea0f
Hendra Susanto <benhanks040888@gmail.com>
2018-01-17 20:37:58 +0700

    add cors middleware

--- routes/api.php
+++ routes/api.php
@@ -4,19 +4,21 @@ use Illuminate\Http\Request;
 
 use App\Score;
 
-Route::get('scores', function (Request $request) {
-  return Score::orderBy('score', 'desc')->take(20)->get();
-});
+Route::middleware(['cors'])->group(function () {
+  Route::get('scores', function (Request $request) {
+    return Score::orderBy('score', 'desc')->take(20)->get();
+  });
 
-Route::post('scores', function (Request $request) {
-  $validator = Validator::make($request->all(), [
-    'name'  => 'required',
-    'score' => 'required'
-  ]);
-  
-  if ($validator->fails()) {
-    return $validator->messages();
-  }
+  Route::post('scores', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+      'name'  => 'required',
+      'score' => 'required'
+    ]);
+    
+    if ($validator->fails()) {
+      return $validator->messages();
+    }
 
-  return Score::create($request->all());
+    return Score::create($request->all());
+  });
 });
